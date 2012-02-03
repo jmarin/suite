0ecaa8fae6b5fc5b149dbc2f35e805228ad7aff8
Juan Marin <juan.marin.otero@gmail.com>
2012-02-03 16:14:53 -0500

    Add JSON Schema validation tests for Catalog Resource and Service Exception

---
+++ geoserver/georest/src/test/java/org/geoserver/geoservices/exception/ServiceExceptionTest.java
@@ -0,0 +1,44 @@
+package org.geoserver.geoservices.exception;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.schema.JsonSchema;
+import org.geoserver.test.GeoServerTestSupport;
+
+/**
+ * GeoServices REST DRAFT 1 Specification - 7.3 Core Exceptions
+ * 
+ * @author Juan Marin
+ * 
+ */
+
+public class ServiceExceptionTest extends GeoServerTestSupport {
+
+    /**
+     * Req. 5: The JSON representation of an exception SHALL validate against the JSON Schema http://schemas.opengis.net/gsr/1.0/exception.json
+     * 
+     * @throws Exception
+     */
+
+    public void testExceptionServiceSchema() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonSchema jsonSchema = mapper.generateJsonSchema(ServiceException.class);
+        JsonNode schemaNode = jsonSchema.getSchemaNode();
+        JsonNode type = schemaNode.get("type");
+        assertEquals("object", type.getTextValue());
+        JsonNode properties = schemaNode.get("properties");
+        JsonNode error = properties.get("error");
+        assertEquals("object", error.get("type").getTextValue());
+        JsonNode errorProperties = error.get("properties");
+        JsonNode code = errorProperties.get("code");
+        assertEquals("string", code.get("type").getTextValue());
+        JsonNode message = errorProperties.get("message");
+        assertEquals("string", message.get("type").getTextValue());
+        JsonNode details = errorProperties.get("details");
+        assertEquals("array", details.get("type").getTextValue());
+        JsonNode detailsItems = details.get("items");
+        assertEquals("string", detailsItems.get("type").getTextValue());
+
+    }
+
+}
--- geoserver/georest/src/test/java/org/geoserver/geoservices/services/CatalogResourceTest.java
+++ geoserver/georest/src/test/java/org/geoserver/geoservices/services/CatalogResourceTest.java
@@ -3,7 +3,12 @@ package org.geoserver.geoservices.services;
 import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.schema.JsonSchema;
 import org.geoserver.geoservices.core.ServiceType;
 import org.geoserver.test.GeoServerTestSupport;
 
@@ -19,6 +24,7 @@ public class CatalogResourceTest extends GeoServerTestSupport {
     /**
      * Req. 1: The Catalog resource SHALL accept requests that conform to the URI template in Table 3 and use any HTTP method identified in the same
      * table (GET) Req. 2: A Catalog resource SHALL support all parameters and values specified in Table 4 (f=json)
+     * 
      */
 
     public void testGetCatalogRootURI() throws Exception {
@@ -27,6 +33,7 @@ public class CatalogResourceTest extends GeoServerTestSupport {
         JSONObject jsonObject = (JSONObject) json;
         assertEquals("1.0", jsonObject.get("specVersion"));
         assertEquals("OpenGeo Suite Enterprise Edition", jsonObject.get("productName"));
+        assertEquals("2.4.4", jsonObject.get("currentVersion"));
         assertEquals(ServiceType.CatalogServer.toString(), jsonObject.get("type"));
         JSONArray folders = (JSONArray) jsonObject.get("folders");
         assertEquals(0, folders.size());
@@ -39,6 +46,7 @@ public class CatalogResourceTest extends GeoServerTestSupport {
      * an exception, if and only if the request violates a request requirement, unless an internal processing exception occurs
      * 
      * This tests for a request with the wrong format
+     * 
      */
     public void testServiceException() throws Exception {
         JSON json = getAsJSON("/rest/services/?f=xxx");
@@ -53,6 +61,44 @@ public class CatalogResourceTest extends GeoServerTestSupport {
         JSONArray details = (JSONArray) error.get("details");
         assertTrue(details instanceof JSONArray);
         assertEquals("Format xxx is not supported", details.getString(0));
+    }
+
+    /**
+     * Req. 3: The JSON representation of the Catalog resource SHALL validate against the JSON Schema
+     * http://schemas.opengis.net/gsr-cs/1.0/catalog.json or in case of an exception against JSON Schema
+     * http://schemas.opengis.net/gsr/1.0/exception.json
+     * 
+     * @throws Exception
+     * 
+     *         NOTE: currentVersion and productVersion are described in the Spec as "number". This would not accomodate versions 1.1.0, 2.4.4, etc.
+     *         Keeping them as String in this implementation
+     */
+    public void testCatalogServiceSchema() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonSchema jsonSchema = mapper.generateJsonSchema(CatalogService.class);
+        JsonNode schemaNode = jsonSchema.getSchemaNode();
+        JsonNode type = schemaNode.get("type");
+        assertEquals("object", type.getTextValue());
+        JsonNode properties = schemaNode.get("properties");
+        JsonNode specVersion = properties.get("specVersion");
+        assertEquals("string", specVersion.get("type").getTextValue());
+        JsonNode productName = properties.get("productName");
+        assertEquals("string", productName.get("type").getTextValue());
+        JsonNode currentVersion = properties.get("currentVersion");
+        assertEquals("string", currentVersion.get("type").getTextValue());
+        JsonNode folders = properties.get("folders");
+        assertEquals("array", folders.get("type").getTextValue());
+        JsonNode folderItems = folders.get("items");
+        assertEquals("string", folderItems.get("type").getTextValue());
+        JsonNode services = properties.get("services");
+        assertEquals("array", services.get("type").getTextValue());
+        JsonNode servicesItems = services.get("items");
+        assertEquals("object", servicesItems.get("type").getTextValue());
+        JsonNode servicesItemsProperties = servicesItems.get("properties");
+        JsonNode serviceName = servicesItemsProperties.get("name");
+        assertEquals("string", serviceName.get("type").getTextValue());
+        JsonNode serviceType = servicesItemsProperties.get("type");
+        assertEquals("string", serviceType.get("type").getTextValue());
     }
 
 }
