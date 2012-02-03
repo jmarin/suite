0e30c58699f8f0c059cc7bb18308082246a5264b
Juan Marin <juan.marin.otero@gmail.com>
2012-02-03 10:48:08 -0500

    Add test for Catalog Exceptions

--- geoserver/georest/src/test/java/org/geoserver/geoservices/catalog/CatalogResourceTest.java
+++ geoserver/georest/src/test/java/org/geoserver/geoservices/catalog/CatalogResourceTest.java
@@ -18,8 +18,7 @@ public class CatalogResourceTest extends GeoServerTestSupport {
 
     /**
      * Req. 1: The Catalog resource SHALL accept requests that conform to the URI template in Table 3 and use any HTTP method identified in the same
-     * table (GET)
-     * Req. 2: A Catalog resource SHALL support all parameters and values specified in Table 4 (f=json)
+     * table (GET) Req. 2: A Catalog resource SHALL support all parameters and values specified in Table 4 (f=json)
      */
 
     public void testGetCatalogRootURI() throws Exception {
@@ -33,7 +32,27 @@ public class CatalogResourceTest extends GeoServerTestSupport {
         assertEquals(5, folders.size());
         JSONArray services = (JSONArray) jsonObject.get("services");
         assertEquals(1, services.size());
+    }
 
+    /**
+     * GeoServices REST DRAFT 1 Specification - 7.3 Exceptions Req. 3: A request to a resource of a Geoservice REST API implementation SHALL result in
+     * an exception, if and only if the request violates a request requirement, unless an internal processing exception occurs
+     * 
+     * This tests for a request with the wrong format
+     */
+    public void testServiceException() throws Exception {
+        JSON json = getAsJSON("/rest/services/?f=xxx");
+        assertTrue(json instanceof JSONObject);
+        JSONObject jsonObject = (JSONObject) json;
+        JSONObject error = (JSONObject) jsonObject.get("error");
+        assertTrue(error instanceof JSONObject);
+        String code = (String) error.get("code");
+        assertEquals("400", code);
+        String message = (String) error.get("message");
+        assertEquals("Bad Request", message);
+        JSONArray details = (JSONArray) error.get("details");
+        assertTrue(details instanceof JSONArray);
+        assertEquals("Format xxx is not supported", details.getString(0));
     }
 
 }
